0714f2ed2f5200fb64afa033bae3e7adba3a8633
Pawel Paszki <ppaszki@redhat.com>
2023-10-12 17:15:03 +0100

    feat: add logger

--- src/controllers/CounterController.js
+++ src/controllers/CounterController.js
@@ -1,22 +1,28 @@
 const { get } = require('lodash');
 const Counter = require('../model/CounterSchema');
+const logger = require('../utils/logger');
 
 exports.IncreaseCounter = async (req, res) => {
-  const ID = get(req, 'body.instanceID');
+  try {
+    const ID = get(req, 'body.instanceID');
 
-  let counter = await Counter.findOne();
-  let count = 1;
-  if (counter !== null) {
-    const currentCounter = counter.counter;
-    count = currentCounter + 1
-    counter.counter = count;
-  } else {
-    counter = new Counter({
-      counter: count,
-    })
+    let counter = await Counter.findOne();
+    let count = 1;
+    if (counter !== null) {
+      const currentCounter = counter.counter;
+      count = currentCounter + 1
+      counter.counter = count;
+    } else {
+      counter = new Counter({
+        counter: count,
+      })
+    }
+
+    await counter.save();
+    logger.info(`counter: ${counter.counter}, instanceId: ${ID}`)
+    await res.send({ counter: counter.counter, instanceId: ID })
+  } catch (error) {
+    logger.error(error)
+    res.send(error)
   }
-
-  await counter.save();
-
-  await res.send({counter: counter.counter, instanceId: ID})
 };
